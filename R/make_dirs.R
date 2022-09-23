53ba00a15b5bec8da878ad68aa6b94eacecd533e
Jake Kaupp <jake.kaupp@queensu.ca>
2022-09-23 14:54:31 -0400

    Added create_irp_dirs

---
+++ R/make_dirs.R
@@ -0,0 +1,44 @@
+check_and_make_dir <- function(root, folder) {
+
+  if (!dir.exists(file.path(root, folder))) {
+
+    invisible(dir.create(file.path(root, folder)))
+
+    cat(crayon::yellow(paste("Created", folder, "Folder\n", sep = " ")))
+
+  } else {
+
+    cat(crayon::yellow(paste(folder, "Folder Already Exists\n", sep = " ")))
+
+  }
+
+}
+
+
+
+#' Function to populate a clean project directory with R, Data, Documentation, Submission
+#' and Correspondence folders
+#'
+#' @param root_dir defaults to here::here() the project root dir
+#'
+#' @export
+#'
+create_irp_dirs <- function (root_dir = here::here())
+{
+
+  root <- root_dir
+
+  if (!dir.exists(root)) {
+    stop("Root Directory Doesn't Exist")
+  }
+
+  folders <- c("R", "Data", "Documentation", "Submission", "Correspondence")
+
+  for (i in folders) {
+
+    check_and_make_dir(root = root, folder = i)
+  }
+
+
+
+}
