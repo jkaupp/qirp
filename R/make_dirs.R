d96c2375498cbaa58eaf4f745e8a423c402f9393
Jake Kaupp <jake.kaupp@queensu.ca>
2022-09-23 15:11:59 -0400

    Updated documentation and examples in create_irp_dirs()

--- R/make_dirs.R
+++ R/make_dirs.R
@@ -17,12 +17,17 @@ check_and_make_dir <- function(root, folder) {
 
 
 #' Function to populate a clean project directory with R, Data, Documentation, Submission
-#' and Correspondence folders
+#' and Correspondence folders.
+#'
+#' Please Note: Unless you run this function in the root project directory, you'll have to supply the path.
 #'
 #' @param root_dir defaults to here::here() the project root dir
 #'
 #' @export
 #'
+#'@examples
+#' # In a project directory
+#' create_irp_dirs()
 create_irp_dirs <- function (root_dir = here::here())
 {
 
