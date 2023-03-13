d0db353819024b4cae8e3469176a5de6e5a4f0b1
Jake Kaupp <jake.kaupp@queensu.ca>
2023-03-13 09:25:35 -0400

    Added irp_path function

---
+++ R/dir_irp.R
@@ -0,0 +1,21 @@
+#' Cross platform file path constructor for IRP Shared Drive
+#'
+#' @param ... character vectors of director or file paths.
+#' @param fsep the path separator to use (assumed to be ASCII).
+#'
+#' @export
+irp_path <- function(..., fsep = .Platform$file.sep) {
+
+  if (.Platform$OS.type != "unix") {
+
+    base_path <- file.path(.Platform$file.sep, "wfs.queensu.ca")
+
+  } else {
+
+    base_path <- file.path(.Platform$file.sep, "Volumes", "PBO")
+  }
+
+  file.path(base_path, list(...), fsep)
+
+
+}
